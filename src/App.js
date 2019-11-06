422eb58bfcaf853c97b4d627f7e305817e2de05b
cmruss <cmrussell08@gmail.com>
2019-11-05 19:09:12 -0500

    MVP design, structure

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import React from "react";
+import React, {useState} from "react";
 import "./App.css";
+import Display from "./components/DisplayComponents/Display";
+import Numbers from "./components/ButtonComponents/NumberButtons/Numbers";
+import Operators from "./components/ButtonComponents/OperatorButtons/Operators";
+import Specials from "./components/ButtonComponents/SpecialButtons/Specials";
 // STEP 4 - import the button and display components
 // Don't forget to import any extra css/scss files you build into the correct component
 
 // Logo has already been provided for you. Do the same for the remaining components
 import Logo from "./components/DisplayComponents/Logo";
 
-function App() {
+function App(props) {
+  const [number, setNumber] = useState('0')
+
+  const handleClick = (event) =>{
+    
+  }
+  
   // STEP 5 - After you get the components displaying using the provided data file, write your state hooks here.
   // Once the state hooks are in place write some functions to hold data in state and update that data depending on what it needs to be doing
   // Your functions should accept a parameter of the the item data being displayed to the DOM (ie - should recieve 5 if the user clicks on
@@ -15,9 +25,19 @@ function App() {
 
   return (
     <div className="container">
+      <div className="logo_container">
       <Logo />
+      </div>
       <div className="App">
         {/* STEP 4 - Render your components here and be sure to properly import/export all files */}
+        <Display />
+        <div className="keypad_container">  
+          <div className="number_column">
+          <Specials />
+          <Numbers />
+          </div>
+          <Operators />
+        </div>
       </div>
     </div>
   );
--- src/components/ButtonComponents/OperatorButtons/OperatorButton.js
+++ src/components/ButtonComponents/OperatorButtons/OperatorButton.js
@@ -1,9 +1,14 @@
 import React from "react";
 
-const OperatorButton = () => {
+const OperatorButton = (props) => {
   return (
     <>
       {/* Display a button element rendering the data being passed down from the parent container on props */}
+      <button onClick ={props.handleClick} className="operator_button" value={props.button.char}>
+        {props.button.char}
+      </button>
     </>
   );
 };
+
+export default OperatorButton;
